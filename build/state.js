abc28102fb03dfb4947bcda870a363edad036b50
Assaf Avital <assaf.avital9@gmail.com>
2024-09-17 13:29:52 +0300

    store last solved date in localStorage (#7)

--- build/state.js
+++ build/state.js
@@ -1,6 +1,6 @@
 class State {
     constructor(s) {
-        this.todaySolved = s.todaySolved;
+        this.lastSolved = s.lastSolved;
         this.streak = s.streak;
         this.totalPlayed = s.totalPlayed;
         this.totalSolved = s.totalSolved;
@@ -14,9 +14,21 @@ export function saveState(state) {
 export function loadState() {
     const state = localStorage.getItem('state');
     return state ? JSON.parse(state) : new State({
-        todaySolved: false,
+        lastSolved: null,
         streak: 0,
         totalPlayed: 0,
         totalSolved: 0,
     });
+}
+
+export function todaySolved(state) {
+    if (state.lastSolved == null) {
+        return false
+    }
+
+    let now = new Date();
+    let lastSolved = new Date(state.lastSolved);
+    return now.getFullYear() === lastSolved.getFullYear() &&
+        now.getMonth() === lastSolved.getMonth() &&
+        now.getDate() === lastSolved.getDate();
 }
